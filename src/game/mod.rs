fb98c7952352749483337f126f1b8faee4df9238
Ryan Chipman <ryan@ryanchipman.com>
2018-12-07 16:30:22 -0500

    Move deal into game module

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -350,3 +350,42 @@ where
         Ok(hand)
     }
 }
+
+pub struct Deal {
+    hand: Hand,
+    dealer: Seat,
+    vulnerable: Vulnerability,
+}
+
+impl Deal {
+    pub fn random() -> Deal {
+        Deal {
+            hand: Hand::random(),
+            dealer: Seat::North,
+            vulnerable: Vulnerability::Neither,
+        }
+    }
+}
+
+impl fmt::Display for Deal {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let dealer = format!("{}", self.dealer);
+        let vulnerable = format!("{}", self.vulnerable);
+
+        let spades = format!("{}", self.hand.suit_holding(Suit::Spades));
+        let hearts = format!("{}", self.hand.suit_holding(Suit::Hearts));
+        let diamonds = format!("{}", self.hand.suit_holding(Suit::Diamonds));
+        let clubs = format!("{}", self.hand.suit_holding(Suit::Clubs));
+
+        writeln!(f, "+-----------------------+")?;
+        writeln!(f, "|     Dealer: {:<10}|", dealer)?;
+        writeln!(f, "+-----------------------+")?;
+        writeln!(f, "| Vulnerable: {:<10}|", vulnerable)?;
+        writeln!(f, "+-----------------------+")?;
+        writeln!(f, "|   Spades: {:<12}|", spades)?;
+        writeln!(f, "|   Hearts: {:<12}|", hearts)?;
+        writeln!(f, "| Diamonds: {:<12}|", diamonds)?;
+        writeln!(f, "|    Clubs: {:<12}|", clubs)?;
+        writeln!(f, "+-----------------------+")
+    }
+}
