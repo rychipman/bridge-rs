06ff04d88764d591a71e568375dc6ab0fc78472a
Ryan Chipman <ryan@ryanchipman.com>
2018-12-13 09:26:25 -0500

    Add tests for valid/invalid bidding sequences

--- src/game/tests.rs
+++ src/game/tests.rs
@@ -68,6 +68,63 @@ test_bidding_finished! {
     pass_out_after_dbl: (vec![ "1S", "Dbl", "Pass", "Pass", "Pass" ], true ),
 }
 
+macro_rules! test_valid_continuation {
+    ($($name:ident: $value:expr,)*) => {
+        mod valid_continuation {
+        $(
+            #[test]
+            fn $name() {
+                use super::super::{BidSequence, Bid};
+                let (bids, next, expected) = $value;
+                let bid_seq = BidSequence::new(bids.into_iter().map(Bid::parse).collect());
+                let next = Bid::parse(next);
+                assert_eq!(bid_seq.valid_continuation(&next), expected);
+            }
+        )*
+        }
+    }
+}
+
+test_valid_continuation! {
+    opening_pass: (vec![], "Pass", true),
+    opening_double: (vec![], "Dbl", false),
+    opening_redouble: (vec![], "Rdbl", false),
+    opening_contract_0: (vec![], "1NT", true),
+    opening_contract_1: (vec![], "7D", true),
+
+    after_contract_pass: (vec!["1S"], "Pass", true),
+    after_contract_double: (vec!["1S"], "Dbl", true),
+    after_contract_redouble: (vec!["1S"], "Rdbl", false),
+    after_contract_contract_0: (vec!["1S"], "1NT", true),
+    after_contract_contract_1: (vec!["1S"], "4S", true),
+    after_contract_too_low_0: (vec!["1S"], "1C", false),
+    after_contract_too_low_1: (vec!["1NT"], "1S", false),
+
+    double_partner: (vec!["1S", "Pass"], "Dbl", false),
+
+    after_double_partner_contract_too_low: (vec!["1S", "Dbl", "Pass"], "1C", false),
+    after_double_partner_contract_valid: (vec!["1S", "Dbl", "Pass"], "2H", true),
+    after_double_partner_pass: (vec!["1S", "Dbl", "Pass"], "Pass", true),
+    after_double_partner_double: (vec!["1S", "Dbl", "Pass"], "Dbl", false),
+    after_double_partner_redouble: (vec!["1S", "Dbl", "Pass"], "Rdbl", false),
+    after_double_redouble: (vec!["1S", "Dbl"], "Rdbl", true),
+    after_double_left_opp_redouble: (vec!["1S", "Dbl", "Pass", "Pass"], "Rdbl", true),
+
+    after_redouble_double: (vec!["1S", "Dbl", "Rdbl"], "Dbl", false),
+    after_redouble_redouble: (vec!["1S", "Dbl", "Rdbl"], "Rdbl", false),
+    after_redouble_contract_too_low_0: (vec!["1S", "Dbl", "Rdbl"], "1S", false),
+    after_redouble_contract_too_low_1: (vec!["1S", "Dbl", "Rdbl"], "1D", false),
+    after_redouble_pass: (vec!["1S", "Dbl", "Rdbl"], "Pass", true),
+    after_redouble_contract_valid: (vec!["1S", "Dbl", "Rdbl"], "2C", true),
+
+    pass_out: (vec!["Pass", "Pass", "Pass"], "Pass", true),
+    already_passed_out_pass: (vec!["Pass", "Pass", "Pass", "Pass"], "Pass", false),
+    already_passed_out_contract: (vec!["Pass", "Pass", "Pass", "Pass"], "1S", false),
+    pass_out_after_contract: (vec!["1S", "Pass", "Pass", "Pass"], "Pass", false),
+    already_passed_out_after_contract: (vec!["1C", "Pass", "Pass", "Pass"], "1S", false),
+    not_passed_out: (vec!["1C", "Dbl", "Pass", "Pass"], "1S", true),
+}
+
 mod compare {
 
     macro_rules! test_bid_compare {
