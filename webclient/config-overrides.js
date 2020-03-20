1f153f20e294f0c0ca3cb9133bc11fbdf34a0a54
Ryan Chipman <ryan@ryanchipman.com>
2020-03-20 15:37:48 -0400

    web: upgrade to latest create-react-app

--- webclient/config-overrides.js
+++ webclient/config-overrides.js
@@ -1,8 +1,7 @@
-const { injectBabelPlugin } = require("react-app-rewired");
-const rewireMobX = require("react-app-rewire-mobx");
-
-module.exports = function override(config, env) {
-	config = injectBabelPlugin("babel-plugin-styled-components", config);
-	config = rewireMobX(config, env);
-	return config
+const { override, disableEsLint, addDecoratorsLegacy } = require("customize-cra");
+module.exports = {
+	webpack: override(
+		addDecoratorsLegacy(),
+		disableEsLint(),
+	),
 };
